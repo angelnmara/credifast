9259d03ff7aede151d80d11fd7d9959e90131fda
dave <angelnmara@hotmail.com>
2017-07-12 23:55:26 -0500

    create database

--- app/src/main/java/space/credifast/credifast/provider/credifastContract.java
+++ app/src/main/java/space/credifast/credifast/provider/credifastContract.java
@@ -4,9 +4,8 @@ import android.content.ContentResolver;
 import android.content.Context;
 import android.net.Uri;
 
-import space.credifast.credifast.R;
-import space.credifast.credifast.interfaces.iTables;
 import space.credifast.credifast.interfaces.iTbUsuColumns;
+import static space.credifast.credifast.interfaces.iTables.tbUsu;
 
 /**
  * Created by angel on 24/06/2017.
@@ -20,14 +19,14 @@ public final class credifastContract {
 
     public static final Uri BASE_CONTENT_URI = Uri.parse("content://" + CONTENT_AUTHORITY);
 
-    static final String PATH_USER = iTables.iTbUsu;
+    static final String PATH_USER = tbUsu;
 
     private credifastContract(){}
 
     public static abstract class cTbUsu implements iTbUsuColumns {
         public static final Uri CONTENT_URI = BASE_CONTENT_URI.buildUpon().appendPath(PATH_USER).build();
-        public static final String CONTENT_TYPE = ContentResolver.CURSOR_DIR_BASE_TYPE + "/vdn." + CONTENT_AUTHORITY + "." + iTables.iTbUsu;
-        public static final String CONTENT_ITEM_TYPE = ContentResolver.CURSOR_ITEM_BASE_TYPE + "/vdn." + CONTENT_AUTHORITY + "." + iTables.iTbUsu;
+        public static final String CONTENT_TYPE = ContentResolver.CURSOR_DIR_BASE_TYPE + "/vdn." + CONTENT_AUTHORITY + "." + tbUsu;
+        public static final String CONTENT_ITEM_TYPE = ContentResolver.CURSOR_ITEM_BASE_TYPE + "/vdn." + CONTENT_AUTHORITY + "." + tbUsu;
         public static final String DEFAULT_SORT = iTbUsuColumns.fcUsu + " COLLATE NOCASE ASC";
 
         public static Uri buildTbUsuUri(String fiIdUsu){
